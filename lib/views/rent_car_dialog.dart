ea6a16b81ebeb2239fe07ddb5d2b6599382e5452
aymen gasmi <aymengasmi25@gmail.com>
2025-07-08 14:51:09 +0100

    adjust rent car dialog

--- lib/views/rent_car_dialog.dart
+++ lib/views/rent_car_dialog.dart
@@ -16,9 +16,17 @@ class _RentCarDialogState extends State<RentCarDialog> {
   final _returnDateController = TextEditingController();
 
   @override
+  void dispose() {
+    _customerController.dispose();
+    _rentDateController.dispose();
+    _returnDateController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text('Rent ${widget.car.name}'),
+      title: Text('Rent ${widget.car.fullName}'),
       content: SingleChildScrollView(
         child: Column(
           children: [
@@ -26,7 +34,16 @@ class _RentCarDialogState extends State<RentCarDialog> {
               readOnly: true,
               decoration: InputDecoration(
                 labelText: 'Car',
-                hintText: widget.car.name,
+                hintText: widget.car.fullName,
+                border: const OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 12),
+            TextField(
+              readOnly: true,
+              decoration: InputDecoration(
+                labelText: 'Plate Number',
+                hintText: widget.car.plateNumber,
                 border: const OutlineInputBorder(),
               ),
             ),
@@ -67,7 +84,9 @@ class _RentCarDialogState extends State<RentCarDialog> {
         ElevatedButton(
           onPressed: () {
             // Save rental to DB
-            print('Renting ${widget.car.name} to ${_customerController.text}');
+            print(
+              'Renting ${widget.car.fullName} (${widget.car.plateNumber}) to ${_customerController.text}',
+            );
             Navigator.pop(context);
           },
           style: ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple),
